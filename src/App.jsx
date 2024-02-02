31f117b3a766573057dfe4e3f2dbb91684177b5a
Devin Raina <devin.raina@gmail.com>
2024-02-02 15:05:50 +0530

    added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,48 @@ function App() {
 
   useEffect(() => {
     fetchData();
-  }, [count]);
+  }, []);
 
   const occup = async () => {
     await updateT();
     setCount(res[0]?.tWash);
+    setTimeout(() => {
+      console.log('Hello Timeout! fetchdata')
+      fetchData();
+   }, 1000);
+   setTimeout(() => {
+    console.log('Hello Timeout! notOccup')
+    notOccup();
+ }, 30000);
   };
 
   const notOccup = async () => {
     await updateF();
     setCount(res[0]?.tWash);
+    setTimeout(() => {
+      console.log('Hello Timeout! fetchdata')
+      fetchData();
+   }, 1000);
   };
+
+ if(count === 'false')
+ {
+  document.body.style.color = 'green';
+ }
+ else if(count === 'true'){
+  document.body.style.color = 'red';
+ }
 
   console.log("count", count);
 
   return (
-    <>
-      <div>
-        Toilet Occupied {`${count}`}
+    <><div>
+      <div className='divt'>
+        Toilet Occupied {`${count}`}<br></br><br />
+        </div>
         <button onClick={occup}>Occupied</button>
         <button onClick={notOccup}>Not Occupied</button>
+        <button onClick={fetchData}>Refresh Status</button>
       </div>
     </>
   );
