50070af68d8f73dcdcab43ff24e8d3ecc75bae24
Devin Raina <devin.raina@gmail.com>
2024-02-02 16:24:54 +0530

    LoadingSpinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from 'react';
 import { findAll, updateT, updateF } from '../src/services/status.mjs';
 import './App.css';
-import { LoadingSpinner } from './services/LoadingSpinner.jsx'
+import LoadingSpinner from './services/LoadingSpinner';
 
 function App() {
   const [count, setCount] = useState(0);
   const [res, setRes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  
   const fetchData = async () => {
     const response = await findAll();
     setRes(response);
     setCount(response[0]?.tWash); 
-    setIsLoading(false);// Optional chaining to handle potential undefined values
+    setTimeout(setIsLoading(false),3000)// Optional chaining to handle potential undefined values
   };
 
   useEffect(() => {
@@ -20,9 +20,9 @@ function App() {
   }, []);
 
   const occup = async () => {
+    setIsLoading(true);
     await updateT();
     setCount(res[0]?.tWash);
-    setIsLoading(true);
     setTimeout(() => {
       console.log('Hello Timeout! fetchdata')
       fetchData();
@@ -34,14 +34,19 @@ function App() {
   };
 
   const notOccup = async () => {
+    setIsLoading(true);
     await updateF();
     setCount(res[0]?.tWash);
-    setIsLoading(true);
     setTimeout(() => {
       console.log('Hello Timeout! fetchdata')
       fetchData();
    }, 1000);
   };
+
+  const refresh= async ()=>{
+    setIsLoading(true);
+    setTimeout( await fetchData(),1000)
+  }
 
  if(count === 'false')
  {
@@ -53,16 +58,22 @@ function App() {
 
   console.log("count", count);
 
-  return (
-    <><div>
+  const donothing=(
+    <div>
       <div className='divt'>
         Toilet Occupied {`${count}`}<br></br><br />
         </div>
-        {isLoading ? <LoadingSpinner /> : alert("status refreshed.")}
-        <button onClick={occup}>Occupied</button>
-        <button onClick={notOccup}>Not Occupied</button>
-        <button onClick={fetchData}>Refresh Status</button>
+        
+        <button onClick={occup}>Occupied</button> &nbsp; 
+        <button onClick={notOccup}>Not Occupied</button> &nbsp; 
+        <button onClick={refresh}>Refresh Status</button>
       </div>
+  );
+
+
+  return (
+    <>
+      {isLoading ? <LoadingSpinner /> : donothing}<br />
     </>
   );
 }
--- src/services/LoadingSpinner.jsx
+++ src/services/LoadingSpinner.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "./spinner.css";
-
+import './spinner.css';
 function LoadingSpinner() {
   return (
     <>
